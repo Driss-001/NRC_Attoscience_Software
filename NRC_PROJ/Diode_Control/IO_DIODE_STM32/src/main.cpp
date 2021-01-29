26dfc88fd15b493cd4e483f5feb315c801212fb6
Driss-001 <drissmann001@gmail.com>
2021-01-29 05:26:38 -0500

    demo is working as intended

--- NRC_PROJ/Diode_Control/IO_DIODE_STM32/src/main.cpp
+++ NRC_PROJ/Diode_Control/IO_DIODE_STM32/src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
+#include "WiFi.h"
 
-int8_t PD1 ;
-int8_t PD2 ;
-int8_t PD3 ;
+
+byte data[2] ; // array to store data
 
 void setup() {
   
-  Serial.begin(115200);
+  Serial.begin(9600);
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
  
   
 
@@ -15,12 +21,10 @@ void setup() {
 
 void loop() {
 
-  PD1 = random(0,10);
-  PD2 = random(0,10);
-  
-  if (Serial.available()>0) {
-    Serial.write(PD1);
-    Serial.write(PD2);
-  }
+  data[0] = random(0,50);
+  data[1] = random(0,50);
+
+  Serial.write(data,sizeof(data));
+  delay(10);
   // put your main code here, to run repeatedly:
 }
--- NRC_PROJ/Diode_Control/Python_Graphs/graph_writer.py
+++ NRC_PROJ/Diode_Control/Python_Graphs/graph_writer.py
@@ -1,12 +1,60 @@
 import numpy as np 
 import math  as mt 
 import matplotlib.pyplot as plt 
+from matplotlib.animation import FuncAnimation
 import serial 
 import time
+import keyboard as kb 
 
-port_name = "COM3"
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = port_name
-ser.reset_input_buffer()
+ser = serial.Serial('COM3')
+ser.baudrate = 9600
+ser.timeout = 1
+n = 25
+SPD1,SPDT,corr = [],[],[]
+
+plt.ion
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.set_xlabel("SPD1")
+ax.set_ylabel("SPDT")
+hl, = ax.plot(SPD1,SPDT,'ro',c= "gray")
+hl2, = ax.plot(SPD1,SPDT,'ro',c= "red")
+
+lines = [hl,hl2]
+
+ax.set_aspect('auto')
+ax.set_xlim(xmin=-1,xmax=2.5)
+ax.set_ylim(ymin=-1,ymax=2.5)
+
+def update_line(new_data):
+   
+    ser.reset_input_buffer()
+    raw_data = ser.read(2)
+    data = list(raw_data)
+    if data[1]==50-data[0]:
+        corr.append(data[0]/n)
+        hl2.set_data(corr,np.ones(len(corr))*2-corr)
+    else:    
+        SPD1.append(data[0]/n)
+        SPDT.append(data[1]/n)
+        hl.set_data(SPD1,SPDT)
+   
+    #plt.pause(0.01)
+    return lines
+
+ani = FuncAnimation(fig,
+                    update_line,
+                    frames=10,
+                    interval=50)
+
+
+
+plt.show()
+
+
+
+
+
+
 
