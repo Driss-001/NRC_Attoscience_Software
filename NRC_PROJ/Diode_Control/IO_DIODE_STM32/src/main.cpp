c72f09e97ca071ab7db2472b2e8032beb03913ab
Driss-001 <drissmann001@gmail.com>
2021-02-12 06:48:42 -0500

    Random float data can now be transmitted from microcontroller to Python thread at 1kHz frequency

--- NRC_PROJ/Diode_Control/IO_DIODE_STM32/src/main.cpp
+++ NRC_PROJ/Diode_Control/IO_DIODE_STM32/src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
 #include "WiFi.h"
+#define SIZE_OF_ARRAY 3
 
+float data[SIZE_OF_ARRAY] ; // array to store data
 
-byte data[2] ; // array to store data
+byte *p = (byte*)data;
+
+char buff[10];
 
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(115200);
   // Set WiFi to station mode and disconnect from an AP if it was previously connected
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -19,12 +23,33 @@ void setup() {
   // put your setup code here, to run once:
 }
 
+
+
+
+
+
 void loop() {
+  //du,,y data-set generator
+  Serial.flush();
+  uint8_t d = random(0,99);
+  float d1 = float(d)/float(100);
+  d = random(0,99);
+  float d2 = float(d)/float(100);
+  d = random(0,99);
+  float d3 = float(d)/float(100);
+  data[0] = float(random(0,3))+d1;
+  data[1] = float(random(0,3))+d2;
+  data[2] = float(random(0,3))+d3;
 
-  data[0] = random(0,50);
-  data[1] = random(0,50);
-
-  Serial.write(data,sizeof(data));
-  delay(10);
+  for(int i=0;i<SIZE_OF_ARRAY;i++){
+    //snprintf (buff, sizeof(buff), "%f", data[i]);
+    Serial.print(data[i]);
+    if(i!=SIZE_OF_ARRAY-1){
+      Serial.print(';');
+      }
+    }
+  Serial.print('\n');
+    delay(1);
+  
   // put your main code here, to run repeatedly:
 }
--- NRC_PROJ/Diode_Control/Python_Graphs/graph_writer.py
+++ NRC_PROJ/Diode_Control/Python_Graphs/graph_writer.py
@@ -4,13 +4,56 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import serial 
 import time
-import keyboard as kb 
+#import keyboard as kb 
+import continuous_threading
+#import ctypes
+import struct,re
+import pandas as pd
+
 
 ser = serial.Serial('COM3')
-ser.baudrate = 9600
+ser.baudrate = 115200
 ser.timeout = 1
 n = 25
-SPD1,SPDT,corr = [],[],[]
+SPD1,SPDT,corr,SPD3 = [],[],[],[]
+Parser = pd.DataFrame()
+ser_data = []
+reading = True
+
+
+ 
+
+def data_thread(delay):
+    for i in range(10):
+        #ser.flush()
+        while(ser.inWaiting()==0):
+                pass
+        try:
+            raw_data = ser.readline().decode("ascii")
+            t_data=[float(val) for val in raw_data.split(';')]
+            SPD1.append(t_data[0])
+            SPDT.append(t_data[1])
+            SPD3.append(t_data[2])
+            
+            ser.flush()
+            time.sleep(delay)
+        except: UnicodeDecodeError    
+        
+        
+        
+
+        
+
+th = continuous_threading.ContinuousThread(target=data_thread,args=[1*10**-3])
+th.start()
+time.sleep(1)
+th.stop()
+
+Parser["SPD1"] = SPD1
+Parser["SPDT"] = SPDT
+Parser["SPD3"] = SPD3
+
+
 
 plt.ion
 
@@ -28,10 +71,9 @@ ax.set_xlim(xmin=-1,xmax=2.5)
 ax.set_ylim(ymin=-1,ymax=2.5)
 
 def update_line(new_data):
-   
-    ser.reset_input_buffer()
-    raw_data = ser.read(2)
-    data = list(raw_data)
+      
+    
+    data = list(new_data)   
     if data[1]==50-data[0]:
         corr.append(data[0]/n)
         hl2.set_data(corr,np.ones(len(corr))*2-corr)
@@ -43,14 +85,15 @@ def update_line(new_data):
     #plt.pause(0.01)
     return lines
 
-ani = FuncAnimation(fig,
-                    update_line,
-                    frames=10,
-                    interval=50)
+#ani = FuncAnimation(fig,
+#                    update_line,
+#                    fargs=ser_data,
+#                    frames=10,
+#                    interval=50)
 
 
 
-plt.show()
+#plt.show()
 
 
 
