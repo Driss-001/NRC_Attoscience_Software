42947fd312acd5af1b5a3d3706eb8cc80195da71
Driss-001 <drissmann001@gmail.com>
2021-02-19 16:06:04 -0500

    time testing done, excel data storing implemented

---
+++ NRC_PROJ/Diode_Control/Python_Graphs/README.md
@@ -0,0 +1,3 @@
+# NRC_Project Python Side
+
+Record data from microcontroller and store them in dataframes then in excel files
--- NRC_PROJ/Diode_Control/Python_Graphs/graph_writer.py
+++ NRC_PROJ/Diode_Control/Python_Graphs/graph_writer.py
@@ -2,102 +2,182 @@ import numpy as np
 import math  as mt 
 import matplotlib.pyplot as plt 
 from matplotlib.animation import FuncAnimation
-import serial 
-import time
+import serial , struct,re ,time,sys
 #import keyboard as kb 
 import continuous_threading
+import threading
 #import ctypes
-import struct,re
 import pandas as pd
 
 
 ser = serial.Serial('COM3')
 ser.baudrate = 115200
 ser.timeout = 1
-n = 25
-SPD1,SPDT,corr,SPD3 = [],[],[],[]
+SPD1,SPDT,corr1,SPD3, corr2,SPD3_corr1, SPD3_corr2 = [],[],[],[],[],[],[]
 Parser = pd.DataFrame()
 ser_data = []
-reading = True
-
-
+ 
+fig = plt.figure()
  
 
-def data_thread(delay):
-    for i in range(10):
+progress_counter = 0
+reading  = False
+ 
+     
+
+
+plt.ion
+
+ax = fig.add_subplot()
+ax.set_xlabel("SPD1")
+ax.set_ylabel("SPDT")
+hl1, = ax.plot(corr1,SPD3_corr1,'ro',c= "gray")
+hl2, = ax.plot(corr2,SPD3_corr2,'ro',c= "red")
+
+ax.set_aspect('auto')
+ax.set_xlim(xmin=-1,xmax=3.1)
+ax.set_ylim(ymin=-1,ymax=3.1)
+   
+lines = [hl1,hl2]
+
+
+        
+
+
+def update_line():
+
+    
+    for i in range(len(SPD1)):
+   
+        if SPD1[i]==SPDT[i]:
+            corr1.append(SPD1[i])
+            SPD3_corr1.append(SPD3[i])
+    
+    hl1.set_data(corr1,SPD3_corr1)
+    
+    for i in range(len(corr1)):
+        if SPD3_corr1[i] ==3-corr1[i]:
+            corr2.append(corr1[i])
+            SPD3_corr2.append(SPD3_corr2[i])
+    
+    hl2.set_data(corr2,SPD3_corr2)
+
+    #plt.pause(0.01)
+    return lines
+
+def Anim_Plot():
+
+    ani = FuncAnimation(fig,
+                    update_line,
+                    fargs=None,
+                    frames=10,
+                    interval=100)
+
+                   
+
+
+def store_data():
+    
+    Parser["SPD1"] = SPD1
+    Parser["SPDT"] = SPDT
+    Parser["SPD3"] = SPD3
+
+    file = "Experiment_Data.xlsx"
+    writer = pd.ExcelWriter(file,engine = 'xlsxwriter') #save results in excel sheets
+    Parser.to_excel(writer,sheet_name='Results') 
+    writer.save()
+    print("Data saved to file")
+#plt.show()
+
+
+
+def data_thread(run_num):
+    #global progress_counter
+    global  progress_counter
+    global reading
+    
+    reading = True
+
+    for i in range(run_num*10**3):
         #ser.flush()
-        while(ser.inWaiting()==0):
-                pass
+        progress_counter += 1
+        #reading = True
         try:
             raw_data = ser.readline().decode("ascii")
             t_data=[float(val) for val in raw_data.split(';')]
             SPD1.append(t_data[0])
             SPDT.append(t_data[1])
             SPD3.append(t_data[2])
-            
-            ser.flush()
-            time.sleep(delay)
         except: UnicodeDecodeError    
-        
-        
-        
 
         
 
-th = continuous_threading.ContinuousThread(target=data_thread,args=[1*10**-3])
-th.start()
-time.sleep(1)
-th.stop()
-
-Parser["SPD1"] = SPD1
-Parser["SPDT"] = SPDT
-Parser["SPD3"] = SPD3
-
-
-
-plt.ion
-
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.set_xlabel("SPD1")
-ax.set_ylabel("SPDT")
-hl, = ax.plot(SPD1,SPDT,'ro',c= "gray")
-hl2, = ax.plot(SPD1,SPDT,'ro',c= "red")
-
-lines = [hl,hl2]
-
-ax.set_aspect('auto')
-ax.set_xlim(xmin=-1,xmax=2.5)
-ax.set_ylim(ymin=-1,ymax=2.5)
-
-def update_line(new_data):
-      
+        #while(ser.inWaiting()==0):
+        #       pass
+        #reading = False
+        ser.flush()
+        time.sleep(10**-3)
     
-    data = list(new_data)   
-    if data[1]==50-data[0]:
-        corr.append(data[0]/n)
-        hl2.set_data(corr,np.ones(len(corr))*2-corr)
-    else:    
-        SPD1.append(data[0]/n)
-        SPDT.append(data[1]/n)
-        hl.set_data(SPD1,SPDT)
-   
-    #plt.pause(0.01)
-    return lines
-
-#ani = FuncAnimation(fig,
-#                    update_line,
-#                    fargs=ser_data,
-#                    frames=10,
-#                    interval=50)
+    reading = False    
+            
 
 
 
-#plt.show()
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
+def main():
+    run_time = 5 #int(sys.argv[1])*60
+    th1 = threading.Thread(target=data_thread,args=(run_time,))
+    #data_thread(run_time)
+    th1.daemon = True
+    #th2.daemon = True
+ 
+    th1.start()
+    th1.join()
+    print(progress_counter)
+    
+    store_data()
+    #printProgressBar(run_time,progress_counter)
 
+    update_line()
 
+    plt.show()
+  
 
+    
+
+    
+
+if __name__ == "__main__":
+    
+    main()
+
+    #try:
+    #    main()
+    #except: 
+    #    if sys.argv[1] == None:
+    #        print("Please input the operation time in minutes(int)")
+    
 
 
 
